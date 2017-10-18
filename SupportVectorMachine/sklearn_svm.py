540af40c77761574b06b4fddf845925b1e111bc7
itmorn <ll@DESKTOP-U4SIVH4>
2017-10-18 10:55:04 +0800

    2

--- SupportVectorMachine/sklearn_svm.py
+++ SupportVectorMachine/sklearn_svm.py
@@ -72,11 +72,17 @@ print "预测正确率: " + str((1.0*rightCount/len(X))*100) + "%"
 
 
 
+'''
+结果：
 
+SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
+  decision_function_shape=None, degree=3, gamma='auto', kernel='rbf',
+  max_iter=-1, probability=False, random_state=None, shrinking=True,
+  tol=0.001, verbose=False)
+测试正确率: 99.0049751244%
+预测正确率: 98.9247311828%
 
-
-
-
+'''
 
 
 
